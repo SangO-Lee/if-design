ebc76a57711bb5cea0aa4765e5e2e2dce4577666
SangO-Lee <ras301@naver.com>
2024-10-16 17:20:33 +0900

    기본 dependency 및 nav, footer component 세팅

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { Reset } from "styled-reset";
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
+
 function App() {
-    return <div className="App">app</div>;
+    return (
+        <>
+            <Reset />
+            <Nav />
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+                <li>4</li>
+            </ul>
+            <Footer />
+        </>
+    );
 }
 
 export default App;
